55c29acd18c9d5c8b842cf34d1a7b6b5150635d2
mikewng <156055657+mikewng@users.noreply.github.com>
2025-07-21 20:47:02 -0400

    first commit

---
+++ src/app/MainScreen/mainscreen.tsx
@@ -0,0 +1,13 @@
+import "./mainscreen.scss"
+
+const MainScreen = () => {
+    return (
+        <div className="main-screen-container">
+            <div className="main-screen-header-container">
+                <h1>Calendar Agent Planner</h1>
+            </div>
+        </div>
+    )
+}
+
+export default MainScreen;
