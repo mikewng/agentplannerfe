338a3a1e3beaaa477df72cb0f7b39d5ce74a4373
mikewng <156055657+mikewng@users.noreply.github.com>
2025-07-27 17:36:09 -0400

    sidebar styling

--- src/app/screens/prompting/components/sidebar.tsx
+++ src/app/screens/prompting/components/sidebar.tsx
@@ -1,18 +1,28 @@
 import "./sidebar.scss"
 import { useNavigation } from "../../../context/navigationContext";
+import { useState } from "react";
 
 const SideBar = () => {
     const { navigate } = useNavigation();
+    const [isCollapsed, setIsCollapsed] = useState(false)
 
     return (
-        <div className="prompting-sidebar-container">
-            <div className="prompting-sidebar-header-container">
-                <div className="sidebar-text">EVA</div>
-                <div className="sidebar-text">{"<<"}</div>
+        <div className={"prompting-sidebar-container" + (isCollapsed ? " collapsed" : "")}>
+            <div className={"prompting-sidebar-header-container" + (isCollapsed ? " collapsed" : "")}>
+                {
+                    !isCollapsed &&
+                    <div className="sidebar-text">EVA</div>
+                }
+                <div className={"sidebar-position-icon" + (isCollapsed ? " collapsed" : "")} onClick={() => setIsCollapsed(!isCollapsed)}></div>
             </div>
             <div className="toolbar-container">
                 <div className="toolbar-body-container">
-                    <div className="toolbar-option" onClick={() => { window.open('https://calendar.google.com/calendar/u/0/r', '_blank') }}>Open Calendar</div>
+                    <div className={"toolbar-option" + (isCollapsed ? " collapsed" : "")} onClick={() => { window.open('https://calendar.google.com/calendar/u/0/r', '_blank') }}>
+                        <div className="option-icon-container">
+                            <div className="option-icon" />
+                        </div>
+                        <div className="option-text">Open Calendar</div>
+                    </div>
                 </div>
                 <div className="toolbar-footer-container">
                     <div className="logout-button" onClick={() => { navigate('main') }}>Logout</div>
